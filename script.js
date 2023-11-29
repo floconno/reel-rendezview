9fc3ad46b8ed54d05ef6e675b575d901e4d03f8b
floconno <floconno@asu.edu>
2023-11-28 20:20:56 -0700

    made script.js file and added a fetch function to call the tmdb api

---
+++ script.js
@@ -0,0 +1,31 @@
+
+/* <form id="birthYearForm">
+    <label for="birthYear">Enter Birth Year:</label>
+    <input type="number" id="birthYear" name="birthYear"/>
+    <button type="button" onclick="fetchMovies()">Generate Movies</button>
+</form> */
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZjY2ZWRlNWUxNGIyYWEyMzM3YjJiN2UwZDc0NTZmNCIsInN1YiI6IjY1NWQ3ZjA3MjY2Nzc4MDEwYzFmNjYzNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VN4gchLL4mhYBHwvjI8lalh-LWaIhHCltxCMT8ripp8'
+    }
+  };
+
+var generateMovies = function fetchMovies() {
+    var birthYear = document.getElementById('birthYear').value;
+    var apiUrl = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&primary_release_year=${birthYear}&sort_by=popularity.desc`
+
+    fetch(apiUrl, options).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayMovies(data);
+            });
+        } else {
+            alert('Error: ' + response.statusText);
+        }
+    })
+    .catch(function (error) {
+        alert('Unable to generate movies');
+    });
+};
